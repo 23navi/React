0fca6f266bba128fcf7bccf2a2ba24eda1b82e08
23navi <navisureka23@gmail.com>
2023-08-04 10:30:34 +0530

    animal project wrapup

--- Stephen/animals/src/AnimalShow.js
+++ Stephen/animals/src/AnimalShow.js
@@ -1,7 +1,37 @@
-function AnimalShow({ type }) {
+import { useState } from "react";
+import bird from "./svg/bird.svg";
+import cat from "./svg/cat.svg";
+import cow from "./svg/cow.svg";
+import dog from "./svg/dog.svg";
+import horse from "./svg/horse.svg";
+import gator from "./svg/gator.svg";
+import heart from "./svg/heart.svg";
+import "./AnimalShow.css";
+
+const svgMap = {
+  bird,
+  cat,
+  cow,
+  dog,
+  horse,
+  gator,
+};
+
+function AnimalShow({ type, index }) {
+  const [like, setLike] = useState(0);
+  const handleLike = () => {
+    setLike(like + 1);
+  };
   return (
-    <div>
+    <div className="animal-show" onClick={handleLike}>
       <h1>{type}</h1>
+      <img className="animal" src={svgMap[type]} alt={type} />
+      <img
+        className="heart"
+        src={heart}
+        alt="heart"
+        style={{ width: 10 + like * 10 + "px" }}
+      />
     </div>
   );
 }
