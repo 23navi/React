84e337e410b1a0a42c2fb177d74996d9ec1b917c
23navi <navisureka23@gmail.com>
2023-08-05 08:33:25 +0530

    BookCreate component

--- Stephen/books/src/App.js
+++ Stephen/books/src/App.js
@@ -1,5 +1,29 @@
-function App(){
-      return <div>App</div>
+import { useState } from "react";
+
+import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
+
+function App() {
+  const [books, useBooks] = useState([]);
+
+  const createBookCallback = (title) => {
+    console.log("BookCreate returned: " + title);
+    // update the books array with the new title received.
+  };
+
+  const deleteBookCallback = (id) => {
+    // update the books array with the new title received.
+  };
+
+  const editBookCallback = (id, title) => {
+    // update the books array with the new title received.
+  };
+
+  return (
+    <div>
+      <BookCreate createBookCallback={createBookCallback} />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- Stephen/books/src/components/BookCreate.js
+++ Stephen/books/src/components/BookCreate.js
@@ -1,5 +1,26 @@
-function BookCreate(){
-      return <div>BookCreate</div>
+import { useState } from "react";
+function BookCreate({ createBookCallback }) {
+  const [title, setTitle] = useState("");
+
+  const handleInputChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    createBookCallback(title);
+    setTitle("");
+  };
+  return (
+    <div className="book-create">
+      <h3>Add a new Book</h3>
+      <form onSubmit={handleFormSubmit}>
+        <label>Title: </label>
+        <input className="input" onChange={handleInputChange} value={title} />
+        <button className="button">Create</button>
+      </form>
+    </div>
+  );
 }
 
-export default BookCreate
+export default BookCreate;
