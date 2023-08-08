5ffd44eb88e5d36d5920e3142606a681d707f697
23navi <navisureka23@gmail.com>
2023-08-08 08:37:35 +0530

    use-effect

--- Stephen/use-effect-practice/src/App.js
+++ Stephen/use-effect-practice/src/App.js
@@ -1,10 +1,19 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 function App() {
   const [counter, setCounter] = useState(0);
+
+  function logCounter() {
+    setCounter(10);
+  }
+
+  const stableLogCounter = useCallback(logCounter, []);
+
   return (
     <div>
       {counter}
       <button onClick={() => setCounter(counter + 1)}>Increment</button>
+      {counter}
+      <button onClick={() => stableLogCounter()}>Console log</button>
     </div>
   );
 }
