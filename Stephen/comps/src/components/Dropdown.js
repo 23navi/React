821acec4ada7ae7303258590163972e6422bc08f
23navi <navisureka23@gmail.com>
2023-08-15 22:07:14 +0530

    dropdown styles

--- Stephen/comps/src/components/Dropdown.js
+++ Stephen/comps/src/components/Dropdown.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { twMerge } from "tailwind-merge";
 function Dropdown({ children, options, value, onClick }) {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -10,7 +11,12 @@ function Dropdown({ children, options, value, onClick }) {
   const renderedOptions = options.map((option) => {
     return (
       <div key={option.value}>
-        <div onClick={() => handleSelect(option)}>{option.label}</div>
+        <div
+          className="hover:bg-sky-100 rounded cursor-pointer p-1"
+          onClick={() => handleSelect(option)}
+        >
+          {option.label}
+        </div>
       </div>
     );
   });
@@ -20,16 +26,23 @@ function Dropdown({ children, options, value, onClick }) {
   }
 
   return (
-    <>
+    <div className="w-48 relative">
       <div
+        className={twMerge(
+          "flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full"
+        )}
         onClick={() => {
           handleClick();
         }}
       >
         {value?.label || "Select....."}
       </div>
-      {isOpen && <div>{renderedOptions}</div>}
-    </>
+      {isOpen && (
+        <div className="absolute top-full border rounded p-3 shadow bg-white w-full">
+          {renderedOptions}
+        </div>
+      )}
+    </div>
   );
 }
 export default Dropdown;
