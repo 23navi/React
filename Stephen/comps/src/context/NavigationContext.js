db70b5f2d0aa5dcc1958dbc23875fb0997f32724
23navi <navisureka23@gmail.com>
2023-08-20 09:01:28 +0530

    programatic navigate function

--- Stephen/comps/src/context/NavigationContext.js
+++ Stephen/comps/src/context/NavigationContext.js
@@ -1,11 +1,30 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const NavigationContext = createContext();
 
 function NavigationProvider({ children }) {
-  const valuesToShare = {};
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+  useEffect(() => {
+    const handlePathChange = () => {
+      setCurrentPath(window.location.pathname);
+    };
+
+    // note then user clicks on arrows in browser history,he navigates to other page.. we don't have to push that using pushState
+    window.addEventListener("popstate", handlePathChange);
+    return () => {
+      window.removeEventListener("popstate", handlePathChange);
+    };
+  }, []);
+
+  const navigate = (to) => {
+    if (to !== currentPath) {
+      window.history.pushState({}, "", to);
+      setCurrentPath(to);
+    }
+  };
+
   return (
-    <NavigationContext.Provider value={valuesToShare}>
+    <NavigationContext.Provider value={{ currentPath, navigate }}>
       {children}
     </NavigationContext.Provider>
   );
