1b40174547f66351122cc77a2ac413d12ec65d43
23navi <navisureka23@gmail.com>
2023-08-24 20:49:44 +0530

    React Portal

--- Stephen/comps/src/components/Modal.js
+++ Stephen/comps/src/components/Modal.js
@@ -1,10 +1,20 @@
+import ReactDOM from "react-dom";
 export default function Modal() {
-  return (
+  return ReactDOM.createPortal(
     <div>
       <div className="absolute inset-0 bg-gray-300 opacity-80"></div>
       <div className="absolute inset-40 p-10 bg-white">
         <div>I'm a modal</div>
       </div>
-    </div>
+    </div>,
+    document.querySelector(".modal-container")
   );
+  // return (
+  //   <div>
+  //     <div className="absolute inset-0 bg-gray-300 opacity-80"></div>
+  //     <div className="absolute inset-40 p-10 bg-white">
+  //       <div>I'm a modal</div>
+  //     </div>
+  //   </div>
+  // );
 }
