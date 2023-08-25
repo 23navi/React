4add7fd46cc3ede6d621b9a72b987ba183abe6f0
23navi <navisureka23@gmail.com>
2023-08-25 19:24:49 +0530

    js sort function

---
+++ Stephen/sortingInJS/test.js
@@ -0,0 +1,53 @@
+/// number sort
+
+// const data = [3, 5, 2];
+// let i = 0;
+// console.log(
+//   data.sort((a, b) => {
+//     i++;
+//     return a - b;
+//   })
+// );
+// console.log(i);
+
+/// string sort
+
+// const data = ["t", "A", "a", "x", "B", "b"];
+// let i = 0;
+// console.log(
+//   data.sort((a, b) => {
+//     return a.localeCompare(b);
+//   })
+// );
+// console.log(i);
+
+/// object sort
+
+// const data = [
+//   { name: "t", age: 3 },
+//   { name: "x", age: 12 },
+//   { name: "y", age: 23 },
+//   { name: "s", age: 6 },
+// ];
+
+// // sort by age
+// const getSortValue = (data) => {
+//   return data.name;
+// };
+
+// const order = "desc";
+
+// data.sort((a, b) => {
+//   const valA = getSortValue(a);
+//   const valB = getSortValue(b);
+
+//   const reverseOrder = order === "desc" ? -1 : 1;
+
+//   if (typeof valA === "string") {
+//     return valA.localeCompare(valB) * reverseOrder;
+//   } else if (typeof valA === "number") {
+//     return (valA - valB) * reverseOrder;
+//   }
+// });
+
+// console.log(data);
