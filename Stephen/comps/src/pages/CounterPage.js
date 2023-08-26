d400458d9652ef7cc2dca111152247664d715bae
23navi <navisureka23@gmail.com>
2023-08-26 12:01:34 +0530

    CounterPage custom hook

--- Stephen/comps/src/pages/CounterPage.js
+++ Stephen/comps/src/pages/CounterPage.js
@@ -1,18 +1,21 @@
 import { useState, useEffect } from "react";
 
-export default function CounterPage({ initialCount }) {
+function useSomething(initialCount = 0) {
   const [count, setCount] = useState(initialCount);
 
   useEffect(() => {
     document.title = `You clicked ${count} times`;
   }, [count]);
 
-  console.log(count);
-
   const handleClick = () => {
     setCount(count + 1);
   };
 
+  return { count, handleClick };
+}
+
+export default function CounterPage({ initialCount }) {
+  const { count, handleClick } = useSomething(initialCount);
   return (
     <div>
       <p>You clicked {count} times</p>
