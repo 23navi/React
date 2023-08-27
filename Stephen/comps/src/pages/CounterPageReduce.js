8d65a9946f830ff1d3e607323d87ff6d37af7138
23navi <navisureka23@gmail.com>
2023-08-27 13:02:26 +0530

    counter with useReducer

--- Stephen/comps/src/pages/CounterPageReduce.js
+++ Stephen/comps/src/pages/CounterPageReduce.js
@@ -2,7 +2,42 @@ import { useReducer } from "react";
 import Button from "../components/Button";
 import Panel from "../components/Panel";
 
-const reducer = (state, action) => {};
+const INCREMENT_VALUE = "increment_value";
+const DECREMENT_VALUE = "decrement_value";
+const VALUE_TO_ADD = "value_to_add";
+const VALUE_TO_CHANGE = "value_to_change";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case INCREMENT_VALUE:
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case DECREMENT_VALUE:
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+
+    case VALUE_TO_ADD:
+      return {
+        ...state,
+        count: state.count + action.payload.valueToAdd,
+        valueToAdd: 0,
+      };
+    case VALUE_TO_CHANGE:
+      return {
+        ...state,
+        valueToAdd: action.payload.valueToAdd,
+      };
+    default:
+      console.error("No matching action type");
+      return {
+        state,
+      };
+  }
+};
 
 function CounterPageReduce({ initialCount }) {
   //   const [count, setCount] = useState(initialCount);
@@ -15,18 +50,26 @@ function CounterPageReduce({ initialCount }) {
 
   const increment = () => {
     //     setCount(count + 1);
+    dispatch({ type: INCREMENT_VALUE });
   };
   const decrement = () => {
     //     setCount(count - 1);
+    dispatch({ type: DECREMENT_VALUE });
   };
+
   const handleChange = (event) => {
     const value = parseInt(event.target.value) || 0;
+    dispatch({ type: VALUE_TO_CHANGE, payload: { valueToAdd: value } });
 
     //     setValueToAdd(value);
   };
   const handleSubmit = (event) => {
     event.preventDefault();
 
+    dispatch({
+      type: VALUE_TO_ADD,
+      payload: { valueToAdd: state.valueToAdd },
+    });
     //     setCount(count + valueToAdd);
     //     setValueToAdd(0);
   };
