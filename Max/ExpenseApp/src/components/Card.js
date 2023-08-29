1cd7da924c4c1431059f0c35935aa1a2933abe45
23navi <navisureka23@gmail.com>
2023-08-29 10:30:35 +0530

    Composition shell components

---
+++ Max/ExpenseApp/src/components/Card.js
@@ -0,0 +1,4 @@
+import "./card.css";
+export default function Card({ children, className }) {
+  return <div className={`card ${className}`}>{children}</div>;
+}
--- Max/ExpenseApp/src/components/ExpenseList.js
+++ Max/ExpenseApp/src/components/ExpenseList.js
@@ -1,14 +1,19 @@
 import ExpenseItem from "./ExpenseItem";
+import "./card.css";
+import "./ExpenseList.css";
 
 export default function ExpenseList({ expenses }) {
   const renderExpenses = expenses.map((expense) => {
     return (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
+      <div className="card">
+        <ExpenseItem
+          className="expenses"
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      </div>
     );
   });
   return <div>{renderExpenses}</div>;
