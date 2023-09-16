0f24468aef2e4ba775d49baa2b177e00ceb377a0
23navi <navisureka23@gmail.com>
2023-09-16 20:30:12 +0530

    rtk-basics from codesandbox

---
+++ Stephen/rtk-basics/src/store/index.js
@@ -0,0 +1,57 @@
+import { configureStore, createSlice, createAction } from "@reduxjs/toolkit";
+
+const reset = createAction("app/reset");
+console.log(reset());
+console.log(reset);
+const songSlice = createSlice({
+  name: "song",
+  initialState: [],
+  reducers: {
+    addSong(state, action) {
+      state.push(action.payload);
+    },
+    removeSong(state, action) {
+      const index = state.indexOf(action.payload);
+      if (index > -1) {
+        // only splice array when item is found
+        state.splice(index, 1);
+      }
+    }
+  }
+});
+
+const movieSlice = createSlice({
+  name: "movie",
+  initialState: [],
+  reducers: {
+    addMovie(state, action) {
+      state.push(action.payload);
+    },
+    removeMovie(state, action) {
+      const index = state.indexOf(action.payload);
+      if (index > -1) {
+        // only splice array when item is found
+        state.splice(index, 1);
+      }
+    }
+  }
+});
+
+const store = configureStore({
+  reducer: {
+    songs: songSlice.reducer,
+    movies: movieSlice.reducer
+  }
+});
+
+// Way one -> Manually
+// store.dispatch({ type: "song/addSong", payload: "navi" });
+
+// Way two -> Taking help from redux-toolkit to create actions
+// store.dispatch(songSlice.actions.addSong("navi2"));
+
+const { addSong, removeSong } = songSlice.actions;
+
+const { addMovie, removeMovie } = movieSlice.actions;
+
+export { store, addSong, removeSong, addMovie, removeMovie };
