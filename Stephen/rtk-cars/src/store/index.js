0f8b16537e038c0ee2dbdfeb476a3547f66d6536
23navi <navisureka23@gmail.com>
2023-09-16 22:33:36 +0530

    setup store

--- Stephen/rtk-cars/src/store/index.js
+++ Stephen/rtk-cars/src/store/index.js
@@ -1,10 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { carsSlice } from "./slices/carsSlice";
+import {
+  carsSlice,
+  addCar,
+  removeCar,
+  changeSearchTerm,
+} from "./slices/carsSlice";
+import { formSlice, changeCost, changeName } from "./slices/formSlice";
 
 const store = configureStore({
   reducer: {
     cars: carsSlice.reducer,
+    form: formSlice.reducer,
   },
 });
 
-export { store };
+export {
+  store,
+  addCar,
+  removeCar,
+  changeSearchTerm,
+  formSlice,
+  changeCost,
+  changeName,
+};
