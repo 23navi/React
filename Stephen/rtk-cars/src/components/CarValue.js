96e0a7cec45161802661aea835f4247289be5220
23navi <navisureka23@gmail.com>
2023-09-18 17:02:03 +0530

    add total sum logic

--- Stephen/rtk-cars/src/components/CarValue.js
+++ Stephen/rtk-cars/src/components/CarValue.js
@@ -1,5 +1,17 @@
+import { useSelector } from "react-redux";
+
 function CarValue() {
-  return <div>CarValue</div>;
+  const totalValue = useSelector((state) => {
+    const { cars, searchTerm } = state.cars;
+    return cars
+      .filter((car) => {
+        return car.name.toLowerCase().includes(searchTerm.toLowerCase());
+      })
+      .reduce((acc, car) => {
+        return acc + car.cost;
+      }, 0);
+  });
+  return <div className="car-value">Total Cost: ${totalValue}</div>;
 }
 
 export default CarValue;
