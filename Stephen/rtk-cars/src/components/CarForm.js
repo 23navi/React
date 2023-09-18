dfb5bc567dc69ad10739c67274a3335b2397a4c4
23navi <navisureka23@gmail.com>
2023-09-18 13:15:05 +0530

    carForm name using store

--- Stephen/rtk-cars/src/components/CarForm.js
+++ Stephen/rtk-cars/src/components/CarForm.js
@@ -1,5 +1,31 @@
+import { useSelector, useDispatch } from "react-redux";
+import { changeCost, changeName } from "../store";
 function CarForm() {
-  return <div>CarForm</div>;
+  const name = useSelector((state) => {
+    return state.form.name;
+  });
+  const dispatch = useDispatch();
+  function handleNameChange(event) {
+    console.log(event.target.value);
+    dispatch(changeName(event.target.value));
+  }
+  return (
+    <div className="car-form panel">
+      <h4 className="subtitle is-3">Add car</h4>
+      <form>
+        <div className="field-group">
+          <div className="field">
+            <label className="label">Name</label>
+            <input
+              className="input is-expanded"
+              value={name}
+              onChange={handleNameChange}
+            ></input>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default CarForm;
