e0e7c0029cc50665bd586bb269f3b39ef80a2d8b
23navi <navisureka23@gmail.com>
2023-10-07 16:45:37 +0530

    pause in fetchUsers

--- Stephen/async-thunks/src/store/thunks/fetchUsers.js
+++ Stephen/async-thunks/src/store/thunks/fetchUsers.js
@@ -1,8 +1,16 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
+const pause = (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+};
 const fetchUsers = createAsyncThunk("users/fetch", async () => {
   const response = await axios.get("http://localhost:3005/users");
   console.log({ response });
+
+  // Dev network request latency
+  await pause(3000); // 3 sec
   return response.data;
 });
 
