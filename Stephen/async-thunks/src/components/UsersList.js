1c5fd4923b48a7a4b3de1a59d98752aa0a062450
23navi <navisureka23@gmail.com>
2023-10-07 16:16:59 +0530

    dispatch in react

--- Stephen/async-thunks/src/components/UsersList.js
+++ Stephen/async-thunks/src/components/UsersList.js
@@ -1,4 +1,11 @@
+import { fetchUsers } from "../store";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 function UsersList() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
   return (
     <div>
       <h1>User Lists</h1>
--- Stephen/async-thunks/src/store/thunks/fetchUsers.js
+++ Stephen/async-thunks/src/store/thunks/fetchUsers.js
@@ -1,3 +1,9 @@
-const fetchUsers = {};
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+const fetchUsers = createAsyncThunk("users/fetch", async () => {
+  const response = await axios.get("http://localhost:3005/users");
+  console.log({ response });
+  return response.data;
+});
 
 export { fetchUsers };
