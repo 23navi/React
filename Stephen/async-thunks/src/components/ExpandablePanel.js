c7fc222f48845a4e7ef68fbdd8bf8a3e7be4023e
23navi <navisureka23@gmail.com>
2023-10-12 18:44:08 +0530

    changes

--- Stephen/async-thunks/src/components/ExpandablePanel.js
+++ Stephen/async-thunks/src/components/ExpandablePanel.js
@@ -3,21 +3,24 @@ import { GoChevronDown, GoChevronUp } from "react-icons/go";
 export default function ExpandablePanel({ children, header }) {
   const [expanded, setExpanded] = useState(false);
 
+  const handleToggleClick = () => {
+    setExpanded(!expanded);
+  };
+
   return (
     <div className="mb-2 border rounded">
       <div className="flex p-2 justify-between items-center">
         <div className="flex flex-row items-center justify-between">
           {header}
         </div>
-        {expanded ? (
-          <div>
-            <GoChevronUp />
-          </div>
-        ) : (
-          <div>
-            <GoChevronDown />
-          </div>
-        )}
+        <div
+          className=" cursor-pointer"
+          onClick={() => {
+            handleToggleClick();
+          }}
+        >
+          {expanded ? <GoChevronUp /> : <GoChevronDown />}
+        </div>
       </div>
       {expanded && children && children.length > 0 && (
         <div className="p-2 border-t"> {children}</div>
