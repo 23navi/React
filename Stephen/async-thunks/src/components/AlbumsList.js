7f9ba1c49bd58425e6411fde4e35e4f3df1ba7a3
23navi <navisureka23@gmail.com>
2023-10-13 23:21:14 +0530

    AlbumsList component layout

--- Stephen/async-thunks/src/components/AlbumsList.js
+++ Stephen/async-thunks/src/components/AlbumsList.js
@@ -1,7 +1,28 @@
 import { useFetchAlbumsQuery } from "../store";
+import Button from "./Button";
+import ExpandablePanel from "./ExpandablePanel";
+import SkeletonLoader from "./SkeletonLoader";
+
 export default function AlbumsList({ user }) {
   const result = useFetchAlbumsQuery(user);
   const { data, isLoading, isFetching, error } = result;
-  console.log({ result });
-  return <div>Albums for {user.name}</div>;
+  let content;
+  if (isFetching) {
+    content = (
+      <SkeletonLoader times={5} className={"h-10 w-100"}></SkeletonLoader>
+    );
+  } else {
+    console.log(data);
+    content = data.map((album) => {
+      const header = <div>{album.title}</div>;
+      return <ExpandablePanel key={album.id} header={header}></ExpandablePanel>;
+    });
+  }
+
+  return (
+    <div>
+      <div>Albums for {user.name}</div>
+      <div>{content}</div>
+    </div>
+  );
 }
