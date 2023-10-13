db0f95bc4c4ad3f7e0676213208cd433f7a99cf1
23navi <navisureka23@gmail.com>
2023-10-13 22:43:00 +0530

    albumsApi setup

---
+++ Stephen/async-thunks/src/store/apis/albumsApi.js
@@ -0,0 +1,25 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const albumsApi = createApi({
+  reducerPath: "albums",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3005" }),
+  endpoints: (builder) => ({
+    // this top query tell that our hook will be use....Query
+    fetchAlbums: builder.query({
+      query: (user) => {
+        return {
+          url: "/albums", // this is the path => base+/albums
+          params: {
+            // Parmas is qs
+            userId: user.id,
+          },
+          method: "GET",
+        };
+      },
+    }),
+  }),
+});
+
+console.log({ albumsApi });
+export const { useFetchAlbumsQuery } = albumsApi;
+export { albumsApi };
