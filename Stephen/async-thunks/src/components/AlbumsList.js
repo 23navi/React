91d128ed628c7295e4d1f8a072f1fe902017e951
23navi <navisureka23@gmail.com>
2023-10-13 22:48:40 +0530

    use of hook in react component init

--- Stephen/async-thunks/src/components/AlbumsList.js
+++ Stephen/async-thunks/src/components/AlbumsList.js
@@ -1,3 +1,7 @@
+import { useFetchAlbumsQuery } from "../store";
 export default function AlbumsList({ user }) {
+  const result = useFetchAlbumsQuery(user);
+  const { data, isLoading, isFetching, error } = result;
+  console.log({ result });
   return <div>Albums for {user.name}</div>;
 }
