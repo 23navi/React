85ffe0e692c7194b884adce49a3a08dfd7930071
23navi <navisureka23@gmail.com>
2023-10-15 02:33:18 +0530

    component refactor

--- Stephen/async-thunks/src/components/PhotosList.js
+++ Stephen/async-thunks/src/components/PhotosList.js
@@ -1,44 +1,42 @@
-import {
-  useAddPhotoMutation,
-  useDeletePhotoMutation,
-  useFetchPhotosQuery,
-} from "../store";
-import Button from "./Button";
-import { GoTrash } from "react-icons/go";
+import { useAddPhotoMutation, useFetchPhotosQuery } from "../store";
+import PhotosListItem from "./PhotosListItem";
 import SkeletonLoader from "./SkeletonLoader";
+import Button from "./Button";
 
 export function PhotosList({ childern, album }) {
   const { data, isLoading, isFetching, error, isError } =
     useFetchPhotosQuery(album);
-  const [deletePhoto, results] = useDeletePhotoMutation();
-  const [addPhoto, results2] = useAddPhotoMutation();
 
-  const handleDelete = () => {
-    deletePhoto(album);
-  };
+  const [addPhoto, addPhotoResults] = useAddPhotoMutation();
 
   const photosList = data?.map((photo) => {
-    return (
-      <div key={photo.id}>
-        <Button
-          className=" mr-3 cursor-pointer"
-          isLoading={results.isLoading}
-          onClick={handleDelete}
-        >
-          <GoTrash />
-        </Button>
-        <div>
-          <img src={photo.url} alt=""></img>
-        </div>
-      </div>
-    );
+    return <PhotosListItem key={photo.key} photo={photo}></PhotosListItem>;
   });
 
+  const handleAddPhoto = () => {
+    addPhoto(album);
+  };
+
+  let content;
+
   if (isLoading || isFetching) {
-    return <SkeletonLoader times={5} className="w-50 h-10"></SkeletonLoader>;
+    content = <SkeletonLoader times={5} className="w-50 h-10"></SkeletonLoader>;
   } else if (isError) {
-    return <p>Error</p>;
+    content = <p>Error</p>;
   } else {
-    return <div>{photosList}</div>;
+    content = <div>{photosList}</div>;
   }
+
+  return (
+    <div>
+      <div className="flex flex-row justify-between m-3">
+        <div>Photos for {album.title}</div>
+        <Button isLoading={addPhotoResults.isLoading} onClick={handleAddPhoto}>
+          + Add Album
+        </Button>
+      </div>
+
+      <div>{content}</div>
+    </div>
+  );
 }
