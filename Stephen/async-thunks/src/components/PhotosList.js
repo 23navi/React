4de380e6d5b98421ee2d591f3f7037e56b999bce
23navi <navisureka23@gmail.com>
2023-10-15 02:19:54 +0530

    fetch photos and delete photos

--- Stephen/async-thunks/src/components/PhotosList.js
+++ Stephen/async-thunks/src/components/PhotosList.js
@@ -1,3 +1,44 @@
+import {
+  useAddPhotoMutation,
+  useDeletePhotoMutation,
+  useFetchPhotosQuery,
+} from "../store";
+import Button from "./Button";
+import { GoTrash } from "react-icons/go";
+import SkeletonLoader from "./SkeletonLoader";
+
 export function PhotosList({ childern, album }) {
-  return <div>Image</div>;
+  const { data, isLoading, isFetching, error, isError } =
+    useFetchPhotosQuery(album);
+  const [deletePhoto, results] = useDeletePhotoMutation();
+  const [addPhoto, results2] = useAddPhotoMutation();
+
+  const handleDelete = () => {
+    deletePhoto(album);
+  };
+
+  const photosList = data?.map((photo) => {
+    return (
+      <div key={photo.id}>
+        <Button
+          className=" mr-3 cursor-pointer"
+          isLoading={results.isLoading}
+          onClick={handleDelete}
+        >
+          <GoTrash />
+        </Button>
+        <div>
+          <img src={photo.url} alt=""></img>
+        </div>
+      </div>
+    );
+  });
+
+  if (isLoading || isFetching) {
+    return <SkeletonLoader times={5} className="w-50 h-10"></SkeletonLoader>;
+  } else if (isError) {
+    return <p>Error</p>;
+  } else {
+    return <div>{photosList}</div>;
+  }
 }
