13ed98e56c617f2e52c0b293de76589028511851
23navi <navisureka23@gmail.com>
2024-01-06 00:29:58 +0530

    webpack server file

--- ReactSSR/src/index.js
+++ ReactSSR/src/index.js
@@ -1,4 +1,10 @@
 const express = require("express");
+
+// Will see a better way to do this later.
+
+const RenderToString = require("react-dom/server").renderToString;
+const Home = require("./client/components/Home").default;
+
 const app = express();
 
 app.get("/", (req, res) => {
